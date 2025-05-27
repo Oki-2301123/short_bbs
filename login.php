267df31841792fee6a8731477ae9f6a30a757698
aso-imamura <2301144@asojuku.ac.jp>
2025-05-27 10:21:13 +0900

    ログイン画面

---
+++ login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h2]>ログイン</h2>
+
+        <from action.="Login.php" method=post>
+            <p>ユーザネーム:<input type="text" name="username" method="post"></p>
+            <p>パスワード<input type="password" name="password" method="post"></p>
+            <p><button type="submit">ログイン</button></p>
+
+
+
+</body>
+
+</html>
