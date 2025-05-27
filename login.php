c2ce2a348933ec6b4d8fafb46fadac320ad129be
aso-imamura <2301144@asojuku.ac.jp>
2025-05-27 10:35:33 +0900

    ログイン画面変更

--- login.php
+++ login.php
@@ -8,13 +8,12 @@
 </head>
 
 <body>
-    <h2]>ログイン</h2>
-
-        <from action.="Login.php" method=post>
-            <p>ユーザネーム:<input type="text" name="username" method="post"></p>
-            <p>パスワード<input type="password" name="password" method="post"></p>
-            <p><button type="submit">ログイン</button></p>
-        </from>
+    <h2>ログイン</h2>
+    <from action.="Login.php" method=post>
+        <p>ユーザネーム:<input type="text" name="username" method="post"></p>
+        <p>パスワード<input type="password" name="password" method="post"></p>
+        <p><button type="submit">ログイン</button></p>
+    </from>
 </body>
 
 </html>
