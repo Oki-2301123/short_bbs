e581b671c3839f660ed9c7c05047a299f2b88685
masuno110 <2301198@s.asojuku.ac.jp>
2025-05-28 11:45:46 +0900

    投稿管理変更2

--- viewkannri.php
+++ viewkannri.php
@@ -10,20 +10,24 @@
     <p><a href="form.php">← 投稿フォームへ戻る</a></p>
     <hr>
     <?php
-    $filename = 'comments.txt';
-    if (file_exists($filename)) {
-        $lines = file($filename, FILE_IGNORE_NEW_LINES);
-        foreach (array_reverse($lines) as $line) {
-            [$time, $name, $comment] = explode("\t", $line);
-            echo "<div class='post'>";
-            echo "<p><strong>$name</strong> さん ($time)</p>";
-            echo "<p>" . nl2br($comment) . "</p>";
-            echo "</div><hr>";
-        }
-    } else {
-        echo "<p>まだ投稿がありません。</p>";
+$filename = 'comments.txt';
+if (file_exists($filename)) {
+    $lines = file($filename, FILE_IGNORE_NEW_LINES);
+    foreach (array_reverse($lines) as $line) {
+        [$id, $time, $name, $comment] = explode("\t", $line);
+        echo "<div class='post'>";
+        echo "<p><strong>$name</strong> さん ($time)</p>";
+        echo "<p>" . nl2br($comment) . "</p>";
+        echo "<form method='post' action='delete.php' onsubmit='return confirm(\"本当に削除しますか？\");'>";
+        echo "<input type='hidden' name='id' value='$id'>";
+        echo "<input type='submit' value='削除'>";
+        echo "</form>";
+        echo "</div><hr>";
     }
-    ?>
+} else {
+    echo "<p>まだ投稿がありません。</p>";
+}
+?>
 </body>
 </html>
 
