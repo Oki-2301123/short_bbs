9d97266595ed601dda18bbe58edb4123a621fed2
haru-aso <2301230@s.asojuku.ac.jp>
2025-05-28 11:17:02 +0900

    Update post.php

--- post.php
+++ post.php
@@ -8,9 +8,10 @@ if (trim($comment) === '') {
     header("Location: form.php");
     exit;
 }
-
+$sql = $pdo->prepare('INSERT INTO comment (user_id, content, created_at) 
+                    VALUES (?, ?, ?)');
+$sql->execute([1, $comment, $time]);
 $entry = "$time\t$name\t$comment\n";
 file_put_contents('comments.txt', $entry, FILE_APPEND);
 header("Location: view.php");
 exit;
-?>
