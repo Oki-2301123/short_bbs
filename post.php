da20bbfba34579bdb62f50f742c52656adaff41e
Oki-2301123 <2301123@s.asojuku.ac.jp>
2025-06-03 10:36:39 +0900

    修正

--- post.php
+++ post.php
@@ -10,15 +10,9 @@ if (trim($comment) === '') {
 }
 //DBに追加
 $pdo = DB();
-var_dump($pdo);
 $sql = $pdo->prepare('INSERT INTO comment (user_id, content) 
                     VALUES (?, ?)');
 $sql->execute([1, $comment]);
-if ($sql) {
-    echo "成功しました";
-} else {
-    echo "失敗しました";
-}
 
 //ファイルに追加
 $entry = "$time\t$name\t$comment\n";
