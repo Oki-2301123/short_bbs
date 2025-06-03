6f23d23b57ffa9f973a90b917092b543ffea0e7f
aso-imamura <2301144@asojuku.ac.jp>
2025-06-03 09:37:55 +0900

    ログインチェックのセッション

---
+++ logincheck.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    session_start();
+    $_SESSION['username'] = $_POST['username'];
+    $_SESSION['password'] = $_POST['password'];
+
+    $pdo = DB();
+    $sql = $pdo->prepare('SELECT * FROM user WHERE username = :username AND password = :password');
+    $sql->bindValue(':username', $_SESSION['username'], PDO::PARAM_STR);
+    $sql->bindValue(':password', $_SESSION['password'], PDO::PARAM_STR);
+    $sql->execute();
+    $result = $sql->fetch(PDO::FETCH_ASSOC);
+    if ($result) {
+        $_SESSION['username'] = $result['username'];
+        $_SESSION['password'] = $result['password'];
+        header("LOcation: form.php");
+    } else {
+        header("Location: Login.php");
+        exit;
+    }
+    ?>
+
+</body>
+
+</html>
